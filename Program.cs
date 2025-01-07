bdaf683177bc6f83f3fa1898b97d61a8f23e37d4
AliHazem3 <147556529+AliHazem3@users.noreply.github.com>
2025-01-07 06:40:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Device_and_Its_Brands.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DevicesString")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicy", policy =>
+    {
+        // Allow both localhost and 127.0.0.1, and without a trailing slash
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();  // Add AllowCredentials if you need to support cookies or authentication
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("MyPolicy");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
